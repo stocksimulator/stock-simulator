c43f31b393b71ead277695280a851e1c7d612d47
twastell <tbwastell@gmail.com>
2020-04-18 15:50:39 -0700

    created conflicts to test

--- server/server.js
+++ server/server.js
@@ -10,12 +10,23 @@ const PORT = 3000;
 // serve static files
 app.use('/build', express.static(path.join(__dirname, '../build')));
 // serve entry point to app (index.html)
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
+
+app.use((err, req, res, next)) => {
+  console.log(err);
+  res.sendStatus(404).json({err: 'An error occurred, your request could not be completed'})
+});
+
 
 console.log(process.env.API_KEY)
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
+})
+
+app.post('user/login', (req, res) => {
+  console.log("blah");
 })
