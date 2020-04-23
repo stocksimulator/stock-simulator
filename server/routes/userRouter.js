36a3f831b9970ccefac1acfcbe14042d03c654f2
Brianna Sookhoo <briannasookhoo@Briannas-MacBook-Air.local>
2020-04-22 19:56:24 -0700

    fixed merge conflicts

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,12 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController.js'); // importing middleware for user login, data and creation
 
-<<<<<<< HEAD
-// post request from client to signup and create new user for app - edited on April 19th 
-router.post('/login', userController.userLogin, (req, res) => {
-  // Check with Tristen for Authentication 
-  res.status(200).json({_id: res.locals.user._id});
-=======
 // post request from client to signup and create new user for app
 router.post('/login', userController.userLogin, (req, res) => {
   res.status(200).json({
@@ -17,8 +11,7 @@ router.post('/login', userController.userLogin, (req, res) => {
     stocks: res.locals.user.stocks,
     success: true,
   });
->>>>>>> 981f3194530b682f75b9c5cbfcaedd214e8be660
-});
+ex});
 
 // post request from client to login to account 
 router.post('/signup', userController.createNewUser, (req,res) => {
@@ -31,11 +24,7 @@ router.post('/signup', userController.createNewUser, (req,res) => {
   });
 });
 
-<<<<<<< HEAD
-// get request from client to access stored user data - added on April 19th
-=======
 // get request from client to access stored user data
->>>>>>> 981f3194530b682f75b9c5cbfcaedd214e8be660
 router.post('/getdata', userController.getUserData, (req, res) => {
   res.status(200).json(res.locals.user);
 });
