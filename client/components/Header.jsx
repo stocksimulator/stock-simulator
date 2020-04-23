8b693c93533ee5d736dc07a6b6eb1fb3fcffd2a3
Brianna Sookhoo <briannasookhoo@Briannas-MacBook-Air.local>
2020-04-22 19:42:55 -0700

    header not working

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,28 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Button from './Button';
 import '../styles/Header.scss';
 
-const Header = () => (
-  <div className='header-container'>
-  <Link to='/'>
-    <div className='logo'>
-      <span>
-        <i className='fas fa-chart-line'></i>Stock Simulator
-      </span>
-    </div>
-  </Link>
-    <div className='options'>
-    <Link to='/app/login'>
-      <Button secondary mr1>
-        Login
-      </Button>
-    </Link>
+const Header = ({userID}) => {
+ console.log('userID: ', userID)
+  if (!userID) {
+    return (
+      <div className='header-container'>
+      <Link to='/'>
+        <div className='logo'>
+          <span>
+            <i className='fas fa-chart-line'></i>Stock Simulator
+          </span>
+        </div>
+      </Link>
+        <div className='options'>
+        <Link to='/app/login'>
+          <Button secondary mr1 >
+            Login
+          </Button>
+        </Link>
+        <Link to='/'>
+          <Button primary >
+            Sign Up
+          </Button>
+        </Link>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className='header-container'>
     <Link to='/'>
-      <Button primary>Sign Up</Button>
+      <div className='logo'>
+        <span>
+          <i className='fas fa-chart-line'></i>Stock Simulator
+        </span>
+      </div>
     </Link>
+      <div className='options'>
+      <Link to='/app/login'>
+        <Button secondary mr1>
+          Logout
+        </Button>
+      </Link>
+      </div>
     </div>
-  </div>
-);
+  );
+}
+
 
 export default Header;
